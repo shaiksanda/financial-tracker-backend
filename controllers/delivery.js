b291e8ffee2e081f074cb54eda327d81f6b01816
shaiksanda <shaiksandanibasha7@gmail.com>
2025-11-03 15:15:20 +0530

    feat(api): build apis for update and delete delivery routes

--- controllers/delivery.js
+++ controllers/delivery.js
@@ -1,4 +1,5 @@
-const Delivery = require("../models/delivery")
+const Delivery = require("../models/delivery");
+const { findById, findByIdAndDelete } = require("../models/user");
 
 module.exports.addDelivery = async (req, res, next) => {
     try {
@@ -34,3 +35,50 @@ module.exports.addDelivery = async (req, res, next) => {
     }
 }
 
+module.exports.updateDelivery = async (req, res, next) => {
+    try {
+        const deliveryId = req.params.id
+        const { id, totalKm, totalOrders, totalEarnings, petrolCost, highestOrderPrice, lowestOrderPrice, notes } = req.body
+        const userId = req.user._id
+
+        const updatedDelivery = {}
+
+        if (totalKm) updatedDelivery.totalKm = totalKm
+
+        if (totalOrders) updatedDelivery.totalOrders = totalOrders;
+        if (totalEarnings) updatedDelivery.totalEarnings = totalEarnings;
+        if (petrolCost) updatedDelivery.petrolCost = petrolCost;
+        if (highestOrderPrice) updatedDelivery.highestOrderPrice = highestOrderPrice;
+        if (lowestOrderPrice) updatedDelivery.lowestOrderPrice = lowestOrderPrice;
+        if (notes) updatedDelivery.notes = notes;
+
+        if (totalEarnings && petrolCost) {
+            updatedDelivery.profit = totalEarnings - petrolCost;
+        }
+
+        const updated = await Delivery.findByIdAndUpdate({ _id: deliveryId, userId }, updatedDelivery, { new: true })
+        if (!updated) {
+            return res.status(401).json({ message: "Delivery Record Not Found or Unauthorized!" })
+        }
+        res.status(200).json({ message: "Delivery Record Updated Successfully!" })
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports.deleteDelivery = async (req, res, next) => {
+    try {
+        const deliveryId = req.params.id
+        const userId = req.user._id
+        const deleted = await Delivery.findOneAndDelete({ _id: deliveryId, userId });
+        if (!deleted) {
+            return res.status(401).json({ message: "Delivery Record Not Found or Unauthorized!" });
+        }
+
+        res.status(200).json({ message: "Delivery Record Deleted Successfully!" })
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
--- routes/delivery.js
+++ routes/delivery.js
@@ -2,8 +2,10 @@ const express=require("express")
 const router=express.Router()
 
 const {authenticateUser}=require("../middlewares/auth")
-const {addDelivery}=require("../controllers/delivery")
+const {addDelivery,updateDelivery,deleteDelivery}=require("../controllers/delivery")
 
 router.post("/add-delivery",authenticateUser,addDelivery)
+router.put("/update-delivery:id",authenticateUser,updateDelivery)
+router.delete("delete-delivery/:id",authenticateUser,deleteDelivery)
 
 export default router
